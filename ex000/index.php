6dbe387a2b151182d63bf38e9e7e35668853bba3
KoderaBit <123130797+koderabit@users.noreply.github.com>
2023-01-29 00:28:10 -0300

    Initial commit

---
+++ ex000/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello world</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            echo "Hello, world! \u{1F30E}";
+        ?>
+    </h1>
+    <h2>Maldição do 'Olá, mundo!'</h2>
+    <p>Conta a lenda que ao aprender uma nova linguagem de programação, o primeiro programa feito deve ser o tradicional 'Hello world', caso contrário o aprendizado não terá sucesso, você vai arriscar?</p>
+</body>
+</html>
